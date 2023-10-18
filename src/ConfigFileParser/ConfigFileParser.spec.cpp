786c818b8b94a308d3d4ebb9129eb1f75d58e3dd
Moa Amaro <moacirjgt@gmail.com>
2023-10-17 21:25:37 -0300

    fix: code style

--- src/ConfigFileParser/ConfigFileParser.spec.cpp
+++ src/ConfigFileParser/ConfigFileParser.spec.cpp
@@ -1,6 +1,6 @@
-#include <gtest/gtest.h>
-
 #include "ConfigFileParser/ConfigFileParser.hpp"
+
+#include <gtest/gtest.h>
 
 TEST(ParserTest, ShouldIdentifyAInvalidServerBlockDefinition) {
   Parser* p = new Parser();
